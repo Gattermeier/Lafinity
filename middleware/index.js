cbefe3e97173339795758ecf32d50e107f1e9cee
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-07-18 17:07:32 -0700

    changed express configuration to avoid deprecated calls

--- middleware/index.js
+++ middleware/index.js
@@ -6,14 +6,16 @@ var morgan = require('morgan');
 
 module.exports = function(app) {
   app.use(morgan('dev'));
-
-
   app.use(cookieParser());
   app.use(session({
-    secret: 'building some awesome ngo services'
+    secret: 'building some awesome ngo services',
+    saveUninitialized: true,
+    resave: true
   }));
-  app.use(bodyParser());
-
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
   // expose session to views
   app.use(function(req, res, next) {
     res.locals.session = req.session;
